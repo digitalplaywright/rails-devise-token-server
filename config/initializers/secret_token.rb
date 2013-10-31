4b8aaa74ec1df36abbb496f2326f23ea430b8200
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2013-10-30 17:11:07 -0700

    initializing to standard rails generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDeviseTokenServer::Application.config.secret_key_base = '13b6b5423f78130c7fdfef569bf4bf848c6b7eb30cf84ef4a6ec88b8a18ed1e6d278c6d90812712690417a65125e372b2203ea7001bb079ff7d74146afbe2053'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDeviseTokenServer::Application.config.session_store :cookie_store, key: '_rails-devise-token-server_session'
